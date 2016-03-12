c11038d3187f177181f04f4bc7be55daf6187c84
carvbapt <carvbapt@gmail.com>
2016-03-12 22:40:09 +0000

    Update Click

--- app/src/main/java/com/example/sauca/project_fc/Splash.java
+++ app/src/main/java/com/example/sauca/project_fc/Splash.java
@@ -1,14 +1,10 @@
 package com.example.sauca.project_fc;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 public class Splash extends AppCompatActivity {
@@ -19,11 +15,10 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton bts = (FloatingActionButton) findViewById(R.id.BT_Splash);
+
+        bts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -36,33 +31,11 @@ public class Splash extends AppCompatActivity {
 
 
     public void  onClick(View v){
-        if(v.getId()==R.id.imageButton){
+        if(v.getId()==R.id.BT_Splash){
             Toast.makeText(this, "Invalid username or password", Toast.LENGTH_LONG).show();
             intent=new Intent(this, com.example.sauca.project_fc.Menu.class);
             startActivity(intent);
 
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
