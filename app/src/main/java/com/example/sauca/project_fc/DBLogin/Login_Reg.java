c301ba78d948810fec241ec6c2fbba4c72bd53a1
carvbapt <carvbapt@gmail.com>
2016-03-14 22:48:42 +0000

    Update DB Login

--- app/src/main/java/com/example/sauca/project_fc/DBLogin/Login_Reg.java
+++ app/src/main/java/com/example/sauca/project_fc/DBLogin/Login_Reg.java
@@ -1,5 +1,8 @@
 package com.example.sauca.project_fc.DBLogin;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -32,6 +35,7 @@ public class Login_Reg extends AppCompatActivity {
         btnList=(Button)findViewById(R.id.BT_List);
 
         addData();
+        viewAll();
 
         Toast.makeText(this, "INICIO DB", Toast.LENGTH_LONG).show();
     }
@@ -41,15 +45,46 @@ public class Login_Reg extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Log.i("Login_Reg","Registo - "+etNome.getText()+" "+etApelido.getText()+" "+etPassword+" "+etEmail);
+                Log.i("Login_Reg", "Registo - " + etNome.getText() + " " + etApelido.getText() + " " + etPassword + " " + etEmail);
 
-               boolean isInserted=myDB.insertData(etNome.getText().toString(),etApelido.getText().toString(),etPassword.getText().toString(),etEmail.getText().toString());
+                boolean isInserted = myDB.insertData(etNome.getText().toString(), etApelido.getText().toString(), etPassword.getText().toString(), etEmail.getText().toString());
 
-                if(isInserted==true)
+                if (isInserted == true)
                     Toast.makeText(Login_Reg.this, "Registo Gravado na BD", Toast.LENGTH_LONG).show();
                 else
                     Toast.makeText(Login_Reg.this, "Registo Não Gravado na BD", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    public void viewAll(){
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Cursor res= myDB.getAllData();
+                    if(res.getCount()==0){
+                        // show message
+                        showMessage("ERRO","Base de Dados vazia");
+                        return;
+
+                    }
+                    StringBuffer buff=new StringBuffer();
+                    while(res.moveToNext()){
+                        buff.append("Id:"+res.getString(0)+"\n");
+                        buff.append("Nome:"+res.getString(1)+"\n");
+                        buff.append("Apelido:"+res.getString(2)+"\n");
+                        buff.append("Email:"+res.getString(4)+"\n");
+                    }
+                    showMessage("Data",buff.toString());
+            }
+        });
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
